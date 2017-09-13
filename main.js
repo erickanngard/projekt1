a23d65b83a26606d85f2bb5e06caf1dbc9e30bae
Eric Känngård <P3UR3@live.se>
2017-09-13 18:29:02 +0200

    eddit side bar

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
 $(document).ready(function(){
+
     $(".sideBar").hide();
+
+    resizeForMobile();
+
+    $(window).resize(function () {
+        resizeForMobile();
+    });
+    
+    
+      
+    
+});
+
+function mobilMenyClicked(){
+    $(".sideBar").show();
+
+    /*$("#mobilMeny").toggle(
+        function(){
+            $("#sideBardiv").css("left", "0px");
+        },
+        function(){
+            $("#sideBardiv").css("left", "200px");
+        }
+    );*/
+}  
+
+function resizeForMobile() {
     var win = $(window).width();
     if (win >= 820) { 
         $(".mobil").hide();
         $(".ejMobil").show();
-     }
+        $(".sideBar").hide();
+    }
     if (win <= 820) {  
         $(".ejMobil").hide();
         $(".mobil").show(); 
     }
-    $(window).resize(function(){
-        var win = $(window).width();
-        if (win >= 820) { 
-            $(".mobil").hide();
-            $(".ejMobil").show();
-         }
-        if (win <= 820) {  
-            $(".ejMobil").hide();
-            $(".mobil").show(); 
-        }
-    });
-});
+}
